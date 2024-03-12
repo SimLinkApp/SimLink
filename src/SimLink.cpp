7d9c8d403df6fbc3426be1b9c21a38def6c0facc
Eamonn Nugent <4699217+space55@users.noreply.github.com>
2024-03-12 02:10:03 -0400

    Boost program options, assets

--- src/SimLink.cpp
+++ src/SimLink.cpp
@@ -16,20 +16,55 @@
 
 #include <thread>
 #include <chrono>
+#include <iostream>
 
-bool debug = false;
+#include <boost/program_options.hpp>
 
-int main()
+namespace po = boost::program_options;
+
+bool run_main_loop = true;
+
+void signal_callback_handler(int signum)
 {
+    switch (signum)
+    {
+    case SIGINT:
+        run_main_loop = false;
+        break;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    bool debug = false;
+    try
+    {
+        po::options_description desc("Allowed options");
+        desc.add_options()("help,h", "print usage message")("verbose,v", po::bool_switch(&debug), "Enable verbose logs");
+
+        po::variables_map vm;
+        store(parse_command_line(argc, argv, desc), vm);
+
+        if (vm.count("help"))
+        {
+            std::cout << desc << "\n";
+            return 0;
+        }
+
+        debug = vm["verbose"].as<bool>();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+
+    signal(SIGINT, signal_callback_handler);
+
     const std::string file_loc = sago::getDataHome() + "/SimLink/simlink.log";
 
     // Initialize Spdlog
     auto file_logger = spdlog::basic_logger_mt("simlink_logger", file_loc);
     // Write messages to logger
-
-#ifdef _DEBUG
-    debug = true;
-#endif
 
     spdlog::set_default_logger(file_logger);
     file_logger->flush_on(spdlog::level::info);
@@ -60,7 +95,7 @@ int main()
     simlink::event_mapper::connect_resources();
     simlink::communication_manager::start_recv_thread();
 
-    while (true)
+    while (run_main_loop)
     {
         simlink::event_mapper::tick();
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
